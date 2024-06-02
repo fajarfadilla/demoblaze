7f22e3a5f36531dbc3495c15849eb6106179c9fc
fajarfadilla <fajarfadillaa@gmail.com>
2024-06-03 00:07:47 +0700

    deleted demoblazeAPI folder

--- demoblazeAPI/playwright.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@playwright/test";
-export default defineConfig({
-  testDir: "./tests",
-  fullyParallel: true,
-  use: {
-    // All requests we send go to this API endpoint.
-    baseURL: "https://api.demoblaze.com",
-  },
-});
